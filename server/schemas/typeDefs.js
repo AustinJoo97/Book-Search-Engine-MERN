637182fc88fc45f87d0de66c2314ede36478950a
AustinJoo97 <austinjoo97@gmail.com>
2021-07-14 14:42:56 -0400

    added Book type def

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,6 +8,15 @@ const typeDefs = gql`
     savedBooks: [Book]!
   }
 
+  type Book {
+      authors: [String],
+      description: [String]!,
+      bookId: String!,
+      image: String,
+      link: String,
+      title: String!
+  }
+
   type Query {
   }
 
