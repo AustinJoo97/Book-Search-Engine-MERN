f6f2e2549850abcb51098c96e936c42ccf8c14d3
AustinJoo97 <austinjoo97@gmail.com>
2021-07-26 15:45:54 -0400

    working to complete typedefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,9 +2,46 @@ const { User } = require('../models');
 
 const resolvers = {
   Query: {
+    getSingleUser: async (parent, args, context) => {
+        const foundUser = await User.findOne({
+          $or: [{ _id: args.user ? args.user._id : args.params.id }, { username: args.params.username }],
+        });
+    
+        if (!foundUser) {
+          return res.status(400).json({ message: 'Cannot find a user with this id!' });
+        }
+      }
   },
 
   Mutation: {
+      createUser: async(parent, { username, email, password }, context) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+        return { token, user };
+      },
+      login: async(parent, args, context) => {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new AuthenticationError('No user found with this email address');
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+
+        const token = signToken(user);
+
+        return { token, user };
+      },
+      saveBook: async(parent, args, context) => {
+
+      },
+      deleteBook: async(parent, args, context) => {
+
+      }
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,25 +2,37 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
+    _id: ID!
     username: String!
     email: String!
     passowrd: String!
-    savedBooks: [Book]!
+    savedBooks: [Book]
   }
 
   type Book {
+      _id: ID!
       authors: [String],
-      description: [String]!,
+      description: String!,
       bookId: String!,
       image: String,
       link: String,
       title: String!
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    getSingleUser(_id: ID, username: String): User
   }
 
   type Mutation {
+      createUser(username: String!, email: String!, password: String!): Auth
+      login(username: String, email: String, password: String): Auth
+      saveBook(_id: ID, authors: [String], description: String!, bookId: String!, image: String, link: String, title: String!): User
+      deleteBook(_id: ID, bookId: String!): User
   }
 `;
 
